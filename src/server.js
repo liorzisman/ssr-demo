a9fb560d91f17f3541aed0cc5b56b18bcf02b47a
liorzisman <liorzisman@gmail.com>
2017-04-26 23:36:14 +0300

    demo-end

--- src/server.js
+++ src/server.js
@@ -1,58 +1,43 @@
-import path from 'path';
 import Express from 'express';
-import React from 'react';
-import { createStore } from 'redux';
-import { Provider } from 'react-redux';
+import React from 'react'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
 import { FeedApp } from './components/FeedApp';
 import { feedApp } from './reducers'
 
-import { renderToString } from 'react-dom/server'
+import { renderToString } from 'react-dom/server';
 
 const app = Express();
 const port = 3000;
 
-//Serve static files
-app.use('/static', Express.static('static'));
-
-// This is fired every time the server side receives a request
-app.use(handleRender);
-
-// We are going to fill these out in the sections to follow
-function handleRender(req, res) {
-  // Create a new Redux store instance
-  const store = createStore(feedApp);
-
-  // Render the component to a string
-  const html = renderToString(
-    <Provider store={store}>
-      <FeedApp />
-    </Provider>
-  );
-
-  // Grab the initial state from our Redux store
-  const preloadedState = store.getState();
-
-  // Send the rendered page back to the client
-  res.send(renderFullPage(html, preloadedState));
-}
-function renderFullPage(html, preloadedState) {
+function renderFullPage(html) {
   return `
-    <!doctype html>
-    <html>
-      <head>
-        <title>Redux Universal Example</title>
-      </head>
-      <body>
-        <div id="root">${html}</div>
-        <script>
-          // WARNING: See the following for security issues around embedding JSON in HTML:
-          // http://redux.js.org/docs/recipes/ServerRendering.html#security-considerations
-          window.__PRELOADED_STATE__ = ${JSON.stringify(preloadedState).replace(/</g, '\\u003c')}
-        </script>
-        <script src="/static/bundle.js"></script>
-      </body>
-    </html>
-    `;
+  <!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <title>Client</title>
+  </head>
+  <body>
+    <div id="root">${html}</div>
+    <script src="/static/bundle.js"></script>
+  </body>
+  </html>
+  `;
 }
+
+function handleRender(req, res) {
+
+  const html = renderToString(
+    <Provider store={createStore(feedApp)}>
+      <FeedApp />
+    </Provider>,
+  )
+
+  res.send(renderFullPage(html));
+}
+
+app.use('/static', Express.static('static'));
+app.use(handleRender);
 
 app.listen(port);
